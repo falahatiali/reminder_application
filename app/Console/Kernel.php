975e77f1bab748aafa4fb89680ab890ba22af584
Ali Agho <ali_fm2009@yahoo.com>
2022-09-04 21:14:25 +0200

    change docker configs. add scheduler container

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,18 +4,19 @@ namespace App\Console;
 
 use App\Library\Reminder\TelegramReminder;
 use App\Models\User;
-use App\Scheduler\SendReminder;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
+        Log::error("STARTTTTTTTTT");
         User::query()->with('reminders')->each(function ($user) use ($schedule) {
             $user->reminders()->each(function ($reminder) use ($user, $schedule) {
                 return $schedule->call(function () use ($reminder) {
-                    $reminderAgent = app(TelegramReminder::class , ['reminder' => $reminder]);
+                    $reminderAgent = app(TelegramReminder::class, ['reminder' => $reminder]);
                     $reminderAgent->SendReminder();
                 })->cron($reminder->expression);
             });
