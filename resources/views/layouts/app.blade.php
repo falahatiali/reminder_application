a205fb7e098c54878cc804b6a7163dd6cf196034
Ali Agho <ali_fm2009@yahoo.com>
2022-09-08 23:49:36 +0200

    chage title for homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Reminder App</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     @livewireStyles
